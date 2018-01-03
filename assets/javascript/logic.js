920153b227910cecc9e1fbe63d6966506920be30
Unionindesign <stevemarshall89@yahoo.com>
2018-01-02 22:56:41 -0700

    mostly there, math nn convert time to mins

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,5 @@
 // alert("logic is linked");
+
 // For timepicker pop-up on form for materialize
   $('.timepicker').pickatime({
     default: 'now', // Set default time: 'now', '1:30AM', '16:30'
@@ -32,7 +33,7 @@
     var destination = $('#destination_name').val().trim();
         console.log("The train goes to: " + destination);
     var frequency = $('#frequency').val().trim();
-        console.log("The train runs every: " + frequency + "minutes.");
+        console.log("The train runs every: " + frequency + " minutes.");
     var firstTime = $('#first_train_time').val().trim();
         console.log("The first train time is: " + firstTime);
     // TODO: make sure you have a value in the form fields for submit to work, use if statement
@@ -58,14 +59,31 @@
     var frequency = childSnapshot.val().frequency;
     var firstTime = childSnapshot.val().firstTime;
 
+    // use childSnapshot to get the value back from the database
+    var trainStart = childSnapshot.val().firstTime;
+    //set year back 1 year to make sure it's before current time
+    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
     // Get current time from Moment.js
     var currentTime = moment();
     console.log("Current Time: " + moment(currentTime).format("hh:mm"));
 
-    // use childSnapshot to get the value back from the database
-    var trainStart = childSnapshot.val().startTime;
-    
-  });
+    var timeDifference = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("Time Difference: " + timeDifference);
+    // in case there's a remainder...
+    var remainder = timeDifference % frequency;
+    console.log(remainder);
+
+    var minsTillTrain = frequency - remainder;
+    console.log("Minutes till train: " + minsTillTrain);
+
+    var nextTrain = moment().add(minsTillTrain, "minutes");
+    console.log("Arrival time: " + moment(nextTrain).format("hh:mm"));
+
+    $("#train-schedule-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency +
+"</td><td>" + nextTrain + "</td></tr>");
+
+});
 
 
   
